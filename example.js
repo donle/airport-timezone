9e073be884145c411277e56d637c224251995b32
donle <donle1988@gmail.com>
2017-02-03 15:59:16 +1100

    initialisation

---
+++ example.js
@@ -0,0 +1,8 @@
+var airport_tz = require('./timezone');
+
+var IATA = 'NYC'
+
+console.log('Searching timezone for: ' + IATA);
+console.log('Timezone: ' + airport_tz.findWhere({ iata: IATA }).get('timezone'));
+console.log('GMT: ' + airport_tz.findWhere({ iata: IATA }).utcOffset());
+
